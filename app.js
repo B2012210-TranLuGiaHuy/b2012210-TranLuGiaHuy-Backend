a6c1e62f29d2fb41cecfa653dc1a643c3bb440cf
huy123789 <tranhuyseven@gmail.com>
2023-02-23 16:54:14 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");                                                                
 const { json } = require("express/lib/response");                                           
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cors());
@@ -10,5 +10,5 @@ app.use(express.json());
 app.get("/" , (req,res)=> {
     res,json({message: "Welcome to contact book application."});
 });
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
